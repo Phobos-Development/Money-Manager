4e862283b40c56a17fe41e9424a246b3442a2903
Jordan <jordansdns@gmail.com>
2022-04-17 02:53:10 +0100

    Add monthly viewing

--- cogs/income_commands.py
+++ cogs/income_commands.py
@@ -6,7 +6,9 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from utils.database_handler import query_sql, create_connection, execute_sql
+from utils.database_handler import create_connection
+from utils.database_handler import execute_sql
+from utils.database_handler import query_sql
 from utils.paginator import Paginator
 
 
@@ -18,82 +20,104 @@ class IncomeCommands(commands.Cog):
     async def build_embeds(data, search_type=None):
         embeds = []
         new_data = {
-            "January": [],
-            "February": [],
-            "March": [],
-            "April": [],
-            "May": [],
-            "June": [],
-            "July": [],
-            "August": [],
-            "September": [],
-            "October": [],
-            "November": [],
-            "December": [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: [],
+            6: [],
+            7: [],
+            8: [],
+            9: [],
+            10: [],
+            11: [],
+            12: [],
         }
         for row in data:
             _, _, year, month, day, amount, note = row
             print(f"[{calendar.month_name[month]} {day}]")
-            new_data[calendar.month_name[month]].append((amount, note))
-        print(new_data)
-        return [discord.Embed(description="123"), discord.Embed(description="1234")]
+            new_data[month].append((amount, note))
+        for month, transactions in new_data.items():
+            embed = discord.Embed(title=calendar.month_name[month], description="")
+            total = sum([tx[0] for tx in transactions])
+            embed.colour = 0xFF0000 if total < 0 else 0x00FF00
+            if len(transactions) == 0:
+                embed.description = (
+                    f"No transactions were made in {calendar.month_name[month]}"
+                )
+            else:
+                embed.description += (
+                    f"`🔴` **Total**: -${total * -1}\n"
+                    if total < 0
+                    else f"`🟢` **Total**: +${total}\n"
+                )
+                for tx in transactions:
+                    embed.description += (
+                        f"\n`🔴` -${tx[0] * -1}" if tx[0] < 0 else f"\n`🟢` +${tx[0]}"
+                    ) + (f" | {tx[1]}" if tx[1] is not None else "")
+            if month <= datetime.now().month:
+                embeds.append(embed)
+        return embeds
 
     @app_commands.command(name="channel")
     @app_commands.guilds(discord.Object(id=853295571448233985))
     async def set_channel(
-            self,
-            interaction: discord.Interaction,
-            channel: discord.TextChannel
+        self, interaction: discord.Interaction, channel: discord.TextChannel
     ):
         current_channel = query_sql(
             create_connection(),
-            f"SELECT channel_id FROM income_channels WHERE discord_id={interaction.user.id}"
+            f"SELECT channel_id FROM income_channels WHERE discord_id={interaction.user.id}",
         )
         if current_channel is None:
             execute_sql(
                 create_connection(),
                 f"INSERT INTO income_channels(discord_id, channel_id) VALUES(?, ?)",
-                (interaction.user.id, channel.id)
+                (interaction.user.id, channel.id),
             )
         else:
             self.bot.channels.remove(current_channel[0])
             execute_sql(
                 create_connection(),
                 f"UPDATE income_channels SET channel_id=? WHERE discord_id=?",
-                (channel.id, interaction.user.id)
+                (channel.id, interaction.user.id),
             )
         self.bot.channels.append(channel.id)
         await interaction.response.send_message(
             embed=discord.Embed(
                 title="Channel Updated!",
                 description=f"I have set your income channel to: {channel.mention}!\n\n"
-                            f"Now you may type `+100 [note]` or `-100 [note]` instead of `/add`"
+                f"Now you may type `+100 [note]` or `-100 [note]` instead of `/add`",
             )
         )
 
     @commands.command(aliases=["spent", "earned"])
     async def total(self, ctx):
         if ctx.invoked_with.lower() == "spent":
-            data = query_sql(create_connection(), "SELECT SUM(amount) FROM income WHERE amount < 0 AND discord_id=?", )
+            data = query_sql(
+                create_connection(),
+                "SELECT SUM(amount) FROM income WHERE amount < 0 AND discord_id=?",
+            )
         elif ctx.invoked_with.lower() == "earned":
-            data = query_sql(create_connection(), "SELECT SUM(amount) FROM income WHERE amount > 0")
+            data = query_sql(
+                create_connection(), "SELECT SUM(amount) FROM income WHERE amount > 0"
+            )
         else:
             data = query_sql(create_connection(), "SELECT SUM(amount) FROM income")
         amt = f"\n`🔴` -${data[0] * -1}" if data[0] < 0 else f"\n`🟢` +${data[0]}"
         await ctx.send(
             embed=discord.Embed(
                 description=f"**Total {ctx.invoked_with.title() if ctx.invoked_with.lower() != 'total' else ''}**: "
-                            f"{amt}",
-                colour=0xFF0000 if data[0] < 0 else 0x00FF00
+                f"{amt}",
+                colour=0xFF0000 if data[0] < 0 else 0x00FF00,
             )
         )
 
     @app_commands.command(name="total")
     @app_commands.guilds(discord.Object(id=853295571448233985))
     async def view_income(
-            self,
-            interaction: discord.Interaction,
-            option: Literal["all", "income", "expenses"]
+        self,
+        interaction: discord.Interaction,
+        option: Literal["all", "income", "expenses"],
     ):
         if option == "all":
             append = ""
@@ -104,14 +128,14 @@ class IncomeCommands(commands.Cog):
         data = query_sql(
             create_connection(),
             f"SELECT SUM(amount) FROM income WHERE discord_id=? {append}",
-            (interaction.user.id,)
+            (interaction.user.id,),
         )
         if data[0] is not None:
             amt = f"\n`🔴` -${data[0] * -1}" if data[0] < 0 else f"\n`🟢` +${data[0]}"
             await interaction.response.send_message(
                 embed=discord.Embed(
                     description=f"**Total {option}**: {amt}",
-                    colour=0xFF0000 if data[0] < 0 else 0x00FF00
+                    colour=0xFF0000 if data[0] < 0 else 0x00FF00,
                 )
             )
         else:
@@ -119,26 +143,30 @@ class IncomeCommands(commands.Cog):
                 embed=discord.Embed(
                     title="Income Manager",
                     description="Run `/help` to get started!",
-                    colour=0x19C7FC
+                    colour=0x19C7FC,
                 )
             )
 
     @app_commands.command(name="monthly")
     @app_commands.guilds(discord.Object(id=853295571448233985))
-    async def view_monthly(
-            self,
-            interaction: discord.Interaction
-    ):
+    async def view_monthly(self, interaction: discord.Interaction):
         data = query_sql(
             create_connection(),
             "SELECT * FROM income WHERE discord_id=? ORDER BY id DESC",
             (interaction.user.id,),
-            one=False
+            one=False,
         )
         embeds = await self.build_embeds(data, "monthly")
         if len(embeds) == 0:
-            embeds.append(discord.Embed(description="No data has been stored yet!", colour=0xFF0000))
-        await interaction.response.send_message(embed=embeds[0], view=Paginator(embeds))
+            embeds.append(
+                discord.Embed(
+                    description="No data has been stored yet!", colour=0xFF0000
+                )
+            )
+        await interaction.response.send_message(
+            embed=embeds[datetime.now().month - 1],
+            view=Paginator(embeds, datetime.now().month - 1),
+        )
 
 
 async def setup(bot):
--- utils/paginator.py
+++ utils/paginator.py
@@ -4,13 +4,19 @@ import discord.ui
 
 
 class Paginator(discord.ui.View):
-    def __init__(self, embeds: List[discord.Embed]):
+    def __init__(self, embeds: List[discord.Embed], start_page):
         super().__init__(timeout=60)
         self.embeds = embeds
-        self.current_page = 0
+        self.current_page = start_page
+        if self.current_page == 0:
+            self.previous_page.disabled = True
+        if self.current_page == len(embeds):
+            self.next_page.disabled = True
 
-    @discord.ui.button(label="←", style=discord.ButtonStyle.blurple, disabled=True)
-    async def previous_page(self, button: discord.ui.Button, interaction: discord.Interaction):
+    @discord.ui.button(label="←", style=discord.ButtonStyle.blurple, disabled=False)
+    async def previous_page(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
         if self.current_page == 1:
             self.previous_page.disabled = True
         self.next_page.disabled = False
@@ -20,7 +26,9 @@ class Paginator(discord.ui.View):
         await interaction.response.edit_message(embed=embed, view=self)
 
     @discord.ui.button(label="→", style=discord.ButtonStyle.blurple, disabled=False)
-    async def next_page(self, button: discord.ui.Button, interaction: discord.Interaction):
+    async def next_page(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
         self.current_page += 1
         print(self.current_page)
         if self.current_page == len(self.embeds) - 1:
